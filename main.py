3fabd4fa690452e5069002e1ff3d989c2342f35c
yagy <logi_st@outlook.jp>
2022-01-07 07:50:25 +0900

    Hide Title bar

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import sys
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from PyQt5 import QtWidgets, uic
  
-class MainWindow(QWidget):
+class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent, Qt.WindowStaysOnTopHint)
+        super(MainWindow, self).__init__(parent)
  
+        uic.loadUi('test.ui', self)
         self.setGeometry(300, 50, 400, 350)
         self.setWindowTitle('QCheckBox')
+        # Stay on top
+        self.setWindowFlag(Qt.WindowStaysOnTopHint)
+        # Hide Title bar
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.show()
  
 if __name__ == '__main__':
     app = QApplication(sys.argv)
